6559254c262f21760630dc6401aa5f531eeb88ba
se7en <269466275@qq.com>
2017-06-20 21:37:08 +0800

    配置文件

---
+++ config/初始化数据库文件.sql
@@ -0,0 +1,16 @@
+DELETE from sys_user where user_ID !=1;
+delete from sys_enterprise where 1=1;
+DELETE from sys_mtree where 1=1;
+DELETE from sys_point where 1=1;
+DELETE from sys_user_role where user_Id !=1;
+DELETE from sys_user_page where user_Id !=1;
+DELETE from sys_point_group where 1=1;
+DELETE from sys_group_point where 1=1;
+DELETE from sys_file where 1=1;
+/** das **/
+DELETE from das_service where 1=1;
+DELETE from dev_group;
+DELETE from dev_info ;
+DELETE from driver_info;
+DELETE from transmit_service;
+DELETE from xml_par;
